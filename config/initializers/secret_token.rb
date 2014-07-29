61a82bec53a9a9eddaf012e48eb5ba3f92e04459
Will Chan <willchan2201@gmail.com>
2014-07-29 16:43:07 -0700

    initializing tutorial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutorialApp::Application.config.secret_key_base = '14724c19aac271a24a3b1a342d0c30088b16f8e0973fc7c5268d40be6196188c712ae984960bd22a2092317ef0ffe11991bb1797f132db51b49cf9ae264d75c4'
