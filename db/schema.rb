d311e8d429479775b6174916b8f5464549e7bba9
Will Chan <willchan2201@gmail.com>
2014-08-10 14:30:00 -0700

    use postgres instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140730174805) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
